812f191b319cbe43a5a12087795f8abd800fdfcf
murali.rajendran <murali.rajendran@veeva.com>
2024-10-01 20:28:45 -0400

    Initial commit

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+    model.addAttribute("title", "Exploring the depths of the ocean in a submarine");
+    model.addAttribute("msg", "This adventure is powered by jellyfish energy and navigated by dolphins");
         return "index";
     }
 
